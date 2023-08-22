c9f84f4eaa63f5b97a60181cf5ef9256ea09e207
Rocki_Kim <rocki.k1208@gmail.com>
2023-08-22 15:50:12 +0900

    FEAT: img upload Guide

--- Introduction_page.py
+++ Introduction_page.py
@@ -25,19 +25,12 @@ def main():
      st.write("사진을 보내주시면, 이메일로 사진 세트를 보내드립니다! 아래는 받아보실 사진의 샘플입니다.")
      st.write("")
      guide_img_paths = load_img_data()
-     st.image(guide_img_paths[4])
+     st.image(guide_img_paths[1])
 
      st.write("## 셀카 사진 업로드 가이드")
-     col1, col2 = st.columns(2)
-     with col1:
-          st.image(guide_img_paths[0],caption='보정이 많이된 사진')
-     with col2:
-          st.image(guide_img_paths[1],caption='과한 표정으로 이목구비가 왜곡된 사진')
-     col1, col2 = st.columns(2)
-     with col1:
-          st.image(guide_img_paths[2],caption='이목구비가 다 안 보이는 사진')
-     with col2:
-          st.image(guide_img_paths[3],caption='이목구비가 틀어진 사진')
+
+     st.image(guide_img_paths[0],caption='보정이 많이된 사진')
+
      want_to_img_upload = st.button("이미지 업로드하러 가기!")
      if want_to_img_upload:
           switch_page("Image_upload_page")
