851dd3d64b99a14eefa0407bc5a5f830fb89d0f0
Rocki_Kim <rocki.k1208@gmail.com>
2023-08-22 14:57:02 +0900

    FEAT: multi image upload
    여러 개 이미지 업로드

--- pages/Image_upload_page.py
+++ pages/Image_upload_page.py
@@ -9,17 +9,18 @@ from google.oauth2 import service_account
 import json
 
 
-def report(region, img, start):
+def report(e_mail, img_lst, start):
     # Authenticate to Firestore with the JSON account key.
     key_dict = json.loads(st.secrets['textkey'])
     creds = service_account.Credentials.from_service_account_info(key_dict)
     db = firestore.Client(credentials=creds)
     result_time = f"{time() - start:.4f}"
     data = {
-        'region': region,
-        'img': img,
-        'datetime': datetime.now(),
-        'time' : result_time
+        'e_mail': e_mail,
+        'selfie_img_1': img_lst[0],
+        'selfie_img_2': img_lst[1],
+        'selfie_img_3': img_lst[2],
+        'datetime': datetime.now()
         }
     
     # Create a reference to the Google post.
@@ -28,11 +29,20 @@ def report(region, img, start):
     return doc_ref[1].id, db, result_time
 
 
-def retrieve(id, db):
+def retrieve_lst(id, db):
     doc_ref = db.collection('test_img').document(id)
     doc = doc_ref.get().to_dict()
-    image_bytes = doc['img']
-    return image_bytes
+    image_bytes_lst = [doc[f'selfie_img_{idx}'] for idx in range(1,4) ]
+    return image_bytes_lst
+
+
+def to_byte_img(uploaded_file):
+    image = Image.open(uploaded_file)
+    buffered = io.BytesIO()
+    image.save(buffered, format="JPEG")
+    byte_image = buffered.getvalue()
+    
+    return byte_image
 
 @st.cache
 def is_valid_email(email):
@@ -53,22 +63,25 @@ def main():
                st.success("성공!")
           else:
                st.warning("잘못된 이메일 형식입니다. 유효한 이메일 주소를 입력하세요.")
+     st.write("## 가이드에 맞는 셀카 사진을 입력해주세요。")
+     uploaded_file_1 = st.file_uploader("첫번째", type=["jpg", "jpeg", "png"])
+     uploaded_file_2 = st.file_uploader("두번째", type=["jpg", "jpeg", "png"])
+     uploaded_file_3 = st.file_uploader("세번째", type=["jpg", "jpeg", "png"])
 
-     uploaded_file = st.file_uploader("가이드에 맞는 셀카 사진을 입력해주세요。", type=["jpg", "jpeg", "png"])
-
-     if uploaded_file is not None and email_input is not None:
-          start_time = time.time()
-          image = Image.open(uploaded_file)
-
-          buffered = io.BytesIO()
-          image.save(buffered, format="JPEG")
-          byte_image = buffered.getvalue()
+     if uploaded_file_1 and uploaded_file_2 and uploaded_file_3 and email_input:
+          start_time = time()
+          byte_imgs = [to_byte_img(file) for file in [uploaded_file_1, uploaded_file_2, uploaded_file_3]]
+          
 
           if st.button("Send"):
-               id, db, result_time = report(email_input, byte_image, start_time)
-               result_img = retrieve(id, db)
-               st.image(result_img, channels="BGR")
-               st.write(result_time + " seconds elapsed.")
+               id, db, result_time = report(email_input, byte_imgs, start_time)
+               result_imgs = retrieve_lst(id, db)
+               st.success("성공!")
+               st.image(result_imgs[0], channels="BGR")
+               st.image(result_imgs[1], channels="BGR")
+               st.image(result_imgs[2], channels="BGR")
+               st.write(result_time + " 업로드 소요시간")
+               
 
 if __name__ == "__main__":
     main()
